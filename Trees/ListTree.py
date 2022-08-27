f57f41e51f76de0e1b5bd722614754254ccee24f
jvp119 <jayvanput@gmail.com>
2022-08-26 22:50:15 -0400

    Tree problem 1022

--- Trees/ListTree.py
+++ Trees/ListTree.py
@@ -17,7 +17,7 @@ def list_to_tree(lst):
     
     while deq and idx < tree_len:
         node = deq.popleft()
-        if validated_lst[idx]:
+        if str(validated_lst[idx]):
             left_child = validated_lst[idx]
             node.left = TreeNode(left_child)
             deq.append(node.left)
